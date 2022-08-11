882c1a71648fc0a165ccec9decb715f7229fc2dd
Sergio San Martin Fuentes <sergio.sanmartin@externos-cl.cencosud.com>
2022-08-10 22:06:04 -0400

    remove pokemon

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -69,8 +69,11 @@ export class PokemonService {
     }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pokemon`;
+  async remove(id: string) {
+    const { deletedCount } = await this.pokemonModel.deleteOne({ _id: id });
+    if (deletedCount === 0)
+      throw new BadRequestException(`Pokemon with id "${id}" not found`);
+    return;
   }
 
   private handleExceptions(error) {
